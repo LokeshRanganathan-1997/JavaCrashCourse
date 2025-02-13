55dcc1c9efb63a5dbc9ec55c7cddedb3847c6927
LokeshRanganathan <138583340+LokeshRanganathan-1997@users.noreply.github.com>
2025-02-13 12:30:33 +0530

    Added new branch LoopingStatements.java

--- ConditionalStatements/LoopingStatements.java
+++ ConditionalStatements/LoopingStatements.java
@@ -5,6 +5,10 @@ public class LoopingStatements {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		for (int Car=1;Car<1; Car++){
+			system.out.println("This is create new branch with Benz");
+		}
+		
 		for(int Tea=0; Tea<10; Tea+=2 ) {
 			System.out.println("Daily Tea drinking 10times and Verify the body condition");
 		}
